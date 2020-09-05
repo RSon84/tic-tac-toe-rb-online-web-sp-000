bc3977d93bc03e56a26230f0d4817bfc0f3d351f
Devin Wilson <craigvin84@yahoo.com>
2020-09-05 17:18:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,7 +83,6 @@ return board[won?(board).first]
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
@@ -96,6 +95,7 @@ end
 
 
 def play(board)
+  puts "Please enter 1-9:"
   turn_count = 0
  while turn_count < 9
    turn(board)
